cc8d1f1cf3647c0c32f7d1666485ef3e90e7dd1c
rofoedu01 <skyline7002@gmail.com>
2025-01-10 22:25:25 -0500

    Use our HCP Terraform organization #5

--- 0-Auth.tf
+++ 0-Auth.tf
@@ -1,19 +1,19 @@
-provider "aws" {
-  region = "us-west-1"
-}
+# provider "aws" {
+#   region = "us-west-1"
+# }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  cloud {
-    organization = "FlemingFriday-RobO"
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+#   cloud {
+#     organization = "FlemingFriday-RobO"
 
-    workspaces {
-      name = "learn-terraform-github-actions"
-    }
-  }
-}
+#     workspaces {
+#       name = "learn-terraform-github-actions"
+#     }
+#   }
+# }
